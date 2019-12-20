36c4a4a6175d8ad60912ec6c8574d4a7b119dab6
Imane Elboutachafaiti <imane.elboutachafaiti@gmail.com>
2019-12-20 12:22:19 +0100

    Rest Api

---
+++ CountdownTimer/Controllers/EventsController.cs
@@ -0,0 +1,28 @@
+﻿using EventDataAccess;
+using System.Linq;
+using System.Web.Http;
+
+namespace CountdownTimer.Controllers
+{
+    [RoutePrefix("api/Events")]
+    public class EventsController : ApiController
+    {
+      
+        [HttpGet]
+        public IHttpActionResult GetEvents()
+        {
+            using (CountDownTimerEntities entities = new CountDownTimerEntities())
+            {
+                return Json( entities.Events.ToList());
+            }
+        }
+
+        public IHttpActionResult GetEvent(int id)
+        {
+            using (CountDownTimerEntities entities = new CountDownTimerEntities())
+            {
+                return Json(entities.Events.FirstOrDefault(e => e.Id == id));
+            }
+        }
+    }
+}
